1b25c4d6919815dbd6a39f672e152e1f831102c9
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2024-01-18 14:52:06 +0900

    justify-between delete

--- src/profile/app/page.tsx
+++ src/profile/app/page.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center">
     {/* <!-- ナビゲーションバー --> */}
     <header className="py-4">
         <div className="container mx-auto flex justify-between items-center px-8 md:px-14 lg:px-14 w-full">
